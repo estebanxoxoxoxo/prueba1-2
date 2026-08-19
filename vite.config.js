d2393c1b46446d67b957b8cb6f629eacfe99edad
esteban <esteban.altamura@gmail.com>
2026-08-19 23:09:36 +0200

    update

--- vite.config.js
+++ vite.config.js
@@ -1,3 +1,5 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 import { eventsSuiteVite } from './events-suite/host/vite.js'
 import { ANALYTICS_WRITE_KEY } from './src/config.js'
